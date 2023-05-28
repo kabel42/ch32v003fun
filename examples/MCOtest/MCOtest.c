e6bcf947de3953c80c1c95b55dcfb1376efdd23f
Jannis Konrad <kabel42@gmail.com>
2023-05-28 11:23:19 +0200

    simple mco test

--- examples/MCOtest/MCOtest.c
+++ examples/MCOtest/MCOtest.c
@@ -6,61 +6,29 @@
 
 int main()
 {
-	uint32_t count, regtemp;
+	// Values lifted from the EVT.  There is little to no documentation on what this does.
+	RCC->CFGR0 = RCC_HPRE_DIV1 | RCC_PLLSRC_HSI_Mul2;
+	RCC->CTLR  = RCC_HSION | RCC_HSEON | RCC_PLLON | ((HSITRIM_DEFAULT) << 3); // Use HSI, but enable PLL.
+	FLASH->ACTLR = FLASH_ACTLR_LATENCY_1;                  // 1 Cycle Latency
+	RCC->INTR  = 0x009F0000;                               // Clear PLL, CSSC, HSE, HSI and LSI ready flags.
 
-	SystemInit48HSI();
-        SetupUART( UART_BRR );
+	// From SetSysClockTo_48MHZ_HSI
+	while((RCC->CTLR & RCC_PLLRDY) == 0);                                      // Wait till PLL is ready
+	RCC->CFGR0 = ( RCC->CFGR0 & ((uint32_t)~(RCC_SW))) | (uint32_t)RCC_SW_PLL; // Select PLL as system clock source
+	while ((RCC->CFGR0 & (uint32_t)RCC_SWS) != (uint32_t)0x08);                // Wait till PLL is used as system clock source
 
-	Delay_Ms(50);
-	printf("\r\r\n\nTesting MCO output options.\r\n");
-	count=0;
+    SetupDebugPrintf();
+	//WaitForDebuggerToAttach();
 
 	RCC->APB2PCENR |= RCC_APB2Periph_GPIOC;
 
-        // PC4 is T1CH4, 50MHz Output PP CNF = 10: Mux PP, MODE = 11: Out 50MHz
-        GPIOC->CFGLR &= ~(GPIO_CFGLR_MODE4 | GPIO_CFGLR_CNF4);
-        GPIOC->CFGLR |= GPIO_CFGLR_CNF4_1 | GPIO_CFGLR_MODE4_0 | GPIO_CFGLR_MODE4_1;
+	// PC4 is T1CH4, 50MHz Output PP CNF = 10: Mux PP, MODE = 11: Out 50MHz
+	GPIOC->CFGLR &= ~(GPIO_CFGLR_MODE4 | GPIO_CFGLR_CNF4);
+	GPIOC->CFGLR |= GPIO_CFGLR_CNF4_1 | GPIO_CFGLR_MODE4_0 | GPIO_CFGLR_MODE4_1;
+
+	RCC->CFGR0 = (RCC->CFGR0 & ~RCC_CFGR0_MCO) | RCC_CFGR0_MCO_PLL;
 
 	while(1)
 	{
-		switch(count)
-		{
-			case 0:
-				printf("\r\nNo signal on MCO\r\n");
-				regtemp = (RCC->CFGR0 & ~RCC_CFGR0_MCO);
-				printf("CFGR0 going from %08lX to %08lX\r\n", RCC->CFGR0, regtemp);
-				RCC->CFGR0 = regtemp;
-				count++;
-				break;
-			case 1:
-				printf("\r\nSYSCLK signal on MCO\r\n");
-				regtemp = (RCC->CFGR0 & ~RCC_CFGR0_MCO) | RCC_CFGR0_MCO_SYSCLK;
-				printf("CFGR0 going from %08lX to %08lX\r\n", RCC->CFGR0, regtemp);
-				RCC->CFGR0 = regtemp;
-				count++;
-				break;
-			case 2:
-				printf("\r\nHSI signal on MCO\r\n");
-				regtemp = (RCC->CFGR0 & ~RCC_CFGR0_MCO) | RCC_CFGR0_MCO_HSI;
-				printf("CFGR0 going from %08lX to %08lX\r\n", RCC->CFGR0, regtemp);
-				RCC->CFGR0 = regtemp;
-				count++;
-				break;
-			case 3:
-				printf("\r\nHSE signal on MCO\r\n");
-				regtemp = (RCC->CFGR0 & ~RCC_CFGR0_MCO) | RCC_CFGR0_MCO_HSE;
-				printf("CFGR0 going from %08lX to %08lX\r\n", RCC->CFGR0, regtemp);
-				RCC->CFGR0 = regtemp;
-				count++;
-				break;
-			case 4:
-				printf("\r\nPLLCLK signal on MCO\r\n");
-				regtemp = (RCC->CFGR0 & ~RCC_CFGR0_MCO) | RCC_CFGR0_MCO_PLL;
-				printf("CFGR0 going from %08lX to %08lX\r\n", RCC->CFGR0, regtemp);
-				RCC->CFGR0 = regtemp;
-				count=0;
-				break;
-		}
-		Delay_Ms(5000);
 	}
 }
